07e7e295b35d861771eaea191553c43d42f2a41c
Robert Crous <robert.crous21@gmail.com>
2020-09-19 14:06:42 +0200

    feat: 🎸 display mobidot info on screen

--- android/src/main/java/com/gometroapp/uma/sample/MainActivity.java
+++ android/src/main/java/com/gometroapp/uma/sample/MainActivity.java
@@ -8,23 +8,33 @@ import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
 import com.gometroapp.uma.client.GoMetroUma;
 import com.gometroapp.uma.util.SentryEvents;
 
 import java.io.File;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Objects;
 
 import io.reactivex.disposables.CompositeDisposable;
 
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 import static com.gometroapp.uma.sample.BuildConfig.APPLICATION_ID;
 
 public final class MainActivity extends AppCompatActivity {
     private static final String TAG = "GoMetroUmaSample";
+
+    private boolean isSdkInitialized = false;
 
     private GoMetroUma goMetroUma;
     private CompositeDisposable disposable = new CompositeDisposable();
@@ -39,21 +49,30 @@ public final class MainActivity extends AppCompatActivity {
 
         // get GoMetroUma instance
         this.goMetroUma = GoMetroUma.getInstance();
-
         ((Button) findViewById(R.id.share_logs))
             .setOnClickListener(this::shareLogs);
+        ((Button) findViewById(R.id.refreshViewFeedback))
+            .setOnClickListener(this::refreshViewFeedback);
+    }
+
+    private void refreshViewFeedback(View view) {
+        refreshTextViews();
+        updateLastRefresh();
     }
 
     @Override
     public void onResume() {
         super.onResume();
+        if (this.goMetroUma == null) {
+            this.goMetroUma = GoMetroUma.getInstance();
+        }
 
         SentryEvents.captureMessage(getClass(), "onResume");
 
         // initialize the GoMetro SDK
         String mainActivityClass = MainActivity.class.getCanonicalName();
         mainActivityClass = mainActivityClass == null ? "" : mainActivityClass;
-
+        Log.i(TAG, "On Resume");
         this.disposable.add(
             this.goMetroUma
                 .init(
@@ -62,8 +81,13 @@ public final class MainActivity extends AppCompatActivity {
                     "kLCHf0LqzG8LVaeEoA3PsqpgEmivYVDycoyGpw86q7NfwlUgxnlfkssVkLxgkf485VMXUYBtmxm1TLh9"
                 )
                 .subscribe(
-                    (unit) -> Log.i(TAG, "SM and Mobidot SDKs started"),
-                    (throwable) -> Log.e(TAG, Objects.requireNonNull(throwable.getMessage()))
+                    (unit) -> {
+                        Log.i(TAG, "SM and Mobidot SDKs started");
+                        this.isSdkInitialized = true;
+                    },
+                    (throwable) -> {
+                        Log.e(TAG, Objects.requireNonNull(throwable.getMessage()));
+                    }
                 )
         );
     }
@@ -110,5 +134,52 @@ public final class MainActivity extends AppCompatActivity {
         } catch (Exception exception) {
             Log.e(TAG, exception.getMessage(), exception);
         }
+    }
+
+    public void updateTextView(int id, String text) {
+        TextView textView = (TextView) findViewById(id);
+        textView.setText(text);
+    }
+    public void updateMobidotUserId() {
+        String mobidotUser = this.goMetroUma.getMobidotUserid();
+        updateTextView(R.id.mobidotUserId, "Mobidot User Id: " + mobidotUser);
+    }
+
+    public void updateFineFeedback() {
+        String status = ContextCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) == PERMISSION_GRANTED ? "Granted" : "Not Granted";
+        updateTextView(R.id.fineLocationPermission, "Fine Location Permission: " + status);
+    }
+
+    public void updateCoarseFeedback() {
+        String status = ContextCompat.checkSelfPermission(this, ACCESS_COARSE_LOCATION) == PERMISSION_GRANTED ? "Granted" : "Not Granted";
+        updateTextView(R.id.coarseLocationPermission, "Coarse Location Permission: " + status);
+    }
+
+    public void updateSensingStateFeedback() {
+        if (! this.isSdkInitialized) return;
+
+        String status = this.goMetroUma.getMeasuringState() ? "On" : "Off";
+        updateTextView(R.id.sensingState, "Sensing State: " + status);
+    }
+
+    public void updateLastRefresh() {
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+        String lastUpdate = df.format(new Date());
+        updateTextView(R.id.lastRefresh, "Last Refresh: " + lastUpdate);
+    }
+
+    public void updateLastTrip() {
+        String start = this.goMetroUma.getLastTripStart();
+        String end = this.goMetroUma.getLastTripEnd();
+        updateTextView(R.id.lastTripStart, "Last trip start: " + start);
+        updateTextView(R.id.lastTripEnd, "Last trip end: " + end);
+    }
+
+    public void refreshTextViews() {
+        updateCoarseFeedback();
+        updateFineFeedback();
+        updateMobidotUserId();
+        updateSensingStateFeedback();
+        updateLastTrip();
     }
 }
